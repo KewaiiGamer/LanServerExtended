4719c5e4747d16711aa0ace48293f53162fdc203
Kewaii <miguelviegas1999@gmail.com>
2018-10-08 13:21:11 +0100

    Increased version number

--- src/main/java/com/kewpanel/lanserverextended/Main.java
+++ src/main/java/com/kewpanel/lanserverextended/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static final String MODID = "lse";
     public static final String MODNAME = "LanServerExtended";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     @Mod.Instance(MODID)
     public static Main Instance = new Main();
